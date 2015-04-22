8b3ef687363516a0c8d61af5d8f4a0a9726e30df
Danilo Sartorelli Barbato <dadlobr@gmail.com>
2015-04-22 00:21:34 +0000

    Ajusta tamanho dos sprites e transforma em botao

--- js/states/edgeMaking.js
+++ js/states/edgeMaking.js
@@ -6,6 +6,7 @@ var edges_state = {
     //vamos para o estado de pathfind
     
     create: function () {
+        var tileSize = 80;
         console.log("edgez");
         var numNodesLocal = document.getElementById("numnodes").value;
         var listaNome = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"];
@@ -13,8 +14,13 @@ var edges_state = {
         var i = 0;
         while(i < numNodesLocal) {
             console.log(numNodesLocal);
-            this.game.add.sprite(128 * Math.round(i / 2) - ( (i % 2) ? 128 : 0), 128 * (i % 2) , 'graphNode');
-            this.game.add.text(128 * Math.round(i / 2) + 60  - ( (i % 2) ? 128 : 0), 128 * (i % 2) + 54, listaNome[i], style);
+            //this.game.add.sprite(128 * Math.round(i / 2) - ( (i % 2) ? 128 : 0), 128 * (i % 2) , 'graphNode');
+            
+            this.game.verticeButton = this.game.add.button(tileSize * Math.round(i / 2) - ( (i % 2) ? tileSize : 0), tileSize * (i % 2) , 'graphNode', this.start, this, 1, 0, 1);
+            this.game.verticeButton.scale.set(0.5);
+            //this.game.verticeButton.anchor.setTo(0.5, 0.5);
+            
+            this.game.add.text(tileSize * Math.round(i / 2) + tileSize/3  - ( (i % 2) ? tileSize : 0), tileSize * (i % 2) + tileSize/3, listaNome[i], style);
             i++;
         }
     },
